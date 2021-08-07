ab20f127c6e74a94d2c3ba4073345664d7bfcd4e
Sajjad Hasanain <saj99h@hotmail.com>
2021-08-07 19:47:56 +0300

    Add CORS & Run app

--- Main.py
+++ Main.py
@@ -1,10 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from Models import session, engine, Base
 from routers import students, insitute_attendance
 
 Base.metadata.create_all(engine)
 
-app = FastAPI()
 
-app.include_router(students.router)
-app.include_router(insitute_attendance.router)
+def create_app(test_config=None):
+    app = FastAPI()
+
+    origins = [
+        "http://localhost",
+        "http://localhost:8080",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    app.include_router(students.router)
+    app.include_router(insitute_attendance.router)
+    return app
+
+
+app = create_app()
